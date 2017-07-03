352c1a8f9adb6334c42357e87050f5895a6814fc
Teddy <teddy.groves@footballradar.com>
2017-07-03 19:30:52 +0100

    pdf-tools and ivy-bibtex

--- init.el
+++ init.el
@@ -12,3 +12,19 @@
 
 (org-babel-load-file (expand-file-name "~/.emacs.d/myinit.org"))
 
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(markdown-command "/usr/local/bin/pandoc" t)
+ '(package-selected-packages
+   (quote
+    (ivy-bibtex pdf-tools dumb-jump counsel-projectile projectile zenburn-theme which-key use-package try stan-snippets ox-reveal org-bullets markdown-mode magit latex-preview-pane ivy-hydra htmlize evil counsel color-theme avy auto-complete)))
+ '(pdf-tools-handle-upgrades nil))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
