60a6364b9acbf87d0790c62449571f9cf6e2938d
Teddy Groves <groves.teddy@gmail.com>
2017-07-03 01:19:37 +0100

    projectile and dumb jump

--- init.el
+++ init.el
@@ -24,9 +24,10 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(org-agenda-files (quote ("~/Documents/Org/organiser/to_do.org")))
  '(package-selected-packages
    (quote
-    (latex-preview-pane markdown-mode stan-snippets evil htmlize zenburn-theme which-key use-package try ox-reveal org-bullets counsel color-theme avy auto-complete))))
+    (dumb-jump counsel-projectile projectile latex-preview-pane markdown-mode stan-snippets evil htmlize zenburn-theme which-key use-package try ox-reveal org-bullets counsel color-theme avy auto-complete))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
--- myinit.el
+++ myinit.el
@@ -98,3 +98,35 @@
 
 (use-package markdown-mode
   :ensure t)
+
+(use-package projectile
+  :ensure t
+  :config
+  (projectile-global-mode)
+(setq projectile-completion-system 'ivy))
+
+(use-package counsel-projectile
+  :ensure t
+  :config
+  (counsel-projectile-on))(use-package projectile
+  :ensure t
+  :config
+  (projectile-global-mode)
+(setq projectile-completion-system 'ivy))
+
+(use-package counsel-projectile
+  :ensure t
+  :config
+  (counsel-projectile-on))
+
+(use-package dumb-jump
+  :bind (("M-g o" . dumb-jump-go-other-window)
+	 ("M-g j" . dumb-jump-go)
+	 ("M-g x" . dumb-jump-go-prefer-external)
+	 ("M-g z" . dumb-jump-go-prefer-external-other-window))
+  :config (setq dumb-jump-selector 'ivy) ;; (setq dumb-jump-selector 'helm)
+
+  :init
+  (dumb-jump-mode)
+    :ensure
+)
