93ba2b0ab50089e5a868ecde1d48fb32b58e68db
Teddy <teddy.groves@footballradar.com>
2017-10-18 10:37:28 +0100

    Add neo tree

--- init.el
+++ init.el
@@ -17,11 +17,11 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(markdown-command "/usr/local/bin/pandoc" t)
+ '(markdown-command "/usr/local/bin/pandoc")
  '(org-agenda-files (quote ("~/Writing/notes/org/away_day_planning.org")))
  '(package-selected-packages
    (quote
-    (ess scala-mode ag flycheck aggressive-indent ivy-bibtex pdf-tools dumb-jump counsel-projectile projectile zenburn-theme which-key use-package try stan-snippets ox-reveal org-bullets markdown-mode magit latex-preview-pane ivy-hydra htmlize evil counsel color-theme avy auto-complete)))
+    (neotree ess scala-mode ag flycheck aggressive-indent ivy-bibtex pdf-tools dumb-jump counsel-projectile projectile zenburn-theme which-key use-package try stan-snippets ox-reveal org-bullets markdown-mode magit latex-preview-pane ivy-hydra htmlize evil counsel color-theme avy auto-complete)))
  '(pdf-tools-handle-upgrades nil))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
